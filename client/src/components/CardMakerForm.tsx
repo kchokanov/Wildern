d16c052f25072c49076e92bdae3d8bb58471fcc3
coo_coo1 <github@chokanov.com>
2024-05-24 18:33:31 +0200

    feat: Add DB write API

--- client/src/components/CardMakerForm.tsx
+++ client/src/components/CardMakerForm.tsx
@@ -1,11 +1,28 @@
 import React, { FC } from 'react'
-import { Box, Text, Center } from '@chakra-ui/react'
+import axios from 'axios'
+import { Box, Text, Center, Button } from '@chakra-ui/react'
+
+async function saveCard(): Promise<void>{
+  axios('http://localhost:3000/api/savecard', {
+    method: "post",
+    data: {name: 'reactCard', _id: '6650619e62ba12950d888020'},
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+  }})
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });}
 
 const CardMakerForm: FC = () => {
   return (
     <Box bg='yellow.200' minW='500px' minH='500px'>
       <Center h='100%'>
         <Text>Form goes here</Text>
+        <Button onClick={saveCard}>Save a card!</Button>
       </Center>
     </Box>
   )
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,46 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
+import cors from 'cors' 
+import bodyParser from 'body-parser'
 import 'dotenv/config'
-import CardEntry from './models/cardEntry'
-import findCardByID from './dbHandler'
+import cardEntry from './models/cardEntry'
 
 const MONGODB_URL: string = process.env.DB_URL ?? ''
 const PORT: number = Number(process.env.PORT ?? 3000)
 
 const app = express()
-const newCard = new CardEntry({
-  name: 'New Card! Pog Champ'
-})
+app.use(cors())
 
-console.log(newCard.id)
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// TODO - fix URL routing
 app.use(express.static(path.join(__dirname, '/../client/dist')))
 
 // Start DB connection then server
 mongoose.connect(MONGODB_URL).then(() => {
   app.listen(PORT, () => {
     console.log(`Web port open on: ${PORT.toString()}`)
-    // Promise.resolve(newCard.save()).catch(err => console.error(err))
-    Promise.resolve(findCardByID(CardEntry, '6650619e62ba12950d888020'))
-      .then(data => console.log(data))
-      .catch(err => console.error(err))
   })
+
+  app.post("/api/savecard", async (req, res) => {
+    try {
+      if(!req.body){res.status(400).send('No data in request')}
+      
+      res.status(200).send('OK')
+
+      // create model from body and save to DB
+      const card = new cardEntry(req.body)
+      cardEntry.findOneAndUpdate({ _id: req.body._id }, req.body, {
+        new: true,
+        upsert: true,
+      })
+      .catch(err => console.error(err))
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Server Error");
+    }
+  });
 }).catch(err => console.error(err))
